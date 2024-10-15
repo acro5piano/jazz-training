f701455c8abc73e81ed2535a2e201a0519f29664
Kay Gosho <ketsume0211@gmail.com>
2024-10-15 18:24:53 +0900

    feat: add code generatorg

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+const KEYS = 'CDEFGAB'.split('')
+
+export function App() {
+  const [codes, setCodes] = useState<string[]>([])
+
+  useEffect(() => {
+    const pid = setInterval(() => {
+      setCodes((codes) => {
+        if (codes.length % 4 === 0) {
+          const rand = Math.floor(10 * Math.random()) % KEYS.length
+          return [...codes, KEYS[rand]]
+        }
+        if (codes.length === 400) {
+          alert('Well Done! OK to restart.')
+          return []
+        }
+        return [...codes, '.']
+      })
+    }, 1000)
+    return () => clearInterval(pid)
+  }, [])
+
+  useEffect(() => {
+    window.scrollTo(0, document.body.scrollHeight)
+  }, [codes.length])
 
   return (
     <>
+      <h1> Jazz Training</h1>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {codes.map((code, i) => (
+          <div key={i}>{code}</div>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
-
-export default App
